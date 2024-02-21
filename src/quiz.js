4f1a89a00ce68f983eed81854755f83675be0461
Michael Hourn <z5419452@ad.unsw.edu.au>
2024-02-21 14:53:41 +1100

    Added stub code for adminQuizCreate, fixed extra file issue

---
+++ src/quiz.js
@@ -0,0 +1,13 @@
+/**
+ * Creates a quiz for the logged in user given basic details
+ * @param {number} authUserId - unique identifier for admin user
+ * @param {string} name - name for the quiz
+ * @param {string} description - description of the quiz
+ * @returns {{quizId: 2}} - quizId
+ */
+
+function adminQuizCreate(authUserId, name, description) {
+    return {
+        quizId: 2
+    };
+}
