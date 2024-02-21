edefd84590a8434fe9837ec5c8a399044b615f16
Abrar Gofur <z5479434@ad.unsw.edu.au>
2024-02-21 15:48:20 +1100

    Adding stub code for AuthRegister

--- src/auth.js
+++ src/auth.js
@@ -26,11 +26,3 @@ function adminAuthRegister( email, password, nameFirst, nameLast ) {
   
   return { authUserId: 1 }
 }
-
-function adminUserDetails( authUserId ) {
-  return { user: { userId: 1,
-     name: 'Hayden Smith',
-      email: 'hayden.smith@unsw.edu.au',
-   numSuccessfulLogins: 3,
-    numFailedPasswordsSinceLastLogin: 1, } }
-}
