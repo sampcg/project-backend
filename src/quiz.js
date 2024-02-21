9e98c1b69a0687c571dca7bb871fddb31e5774c8
Sam <z5363931@ad.unsw.edu.au>
2024-02-21 11:48:52 +1100

    Write stub code for adminQuizNameUpdate

--- src/quiz.js
+++ src/quiz.js
@@ -1,27 +1,13 @@
 // 
 /**
+ *
  * @param {number} authUserId - unique identifier for an authorated user
  * @param {number} quizId - unique identifier for quiz 
- * @returns {{academic: {name: string, hobby: string}}}
- * @returns {Object} - An object containing quiz details.
- *                    - Returns null if quiz is not found or user is not authorized.
- *                    - Example return object:
- *                      {
- *                          quizId: 1,
- *                          name: 'My Quiz',
- *                          timeCreated: 1683125870,
- *                          timeLastEdited: 1683125871,
- *                          description: 'This is my quiz',
- *                      }
- * 
- */
-function adminQuizInfo(authUserId, quizId ) {
-    // Program to get all of the relevant information about the current quiz.
-   return { 
-        quizId: 1, 
-        name: 'My Quiz', 
-        timeCreated: 1683125870,
-        timeLastEdited: 1683125871, 
-        description: 'This is my quiz', 
-    };
+* @param {string} name - updated name for relevant quiz
+ * @returns {} an empty object
+*/
+
+function adminQuizNameUpdate( authUserId, quizId, name ) {
+	return {
+	};
 }
