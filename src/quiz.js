45fb417b74c62ce260df8c90d7f24a547bbabc62
Sam <z5363931@ad.unsw.edu.au>
2024-02-21 16:16:51 +1100

    Modified return section to return variables

--- src/quiz.js
+++ src/quiz.js
@@ -3,16 +3,7 @@
  * @param {number} authUserId - unique identifier for an authorated user
  * @param {number} quizId - unique identifier for quiz 
  * @returns {{academic: {name: string, hobby: string}}}
- * @returns {Object} - An object containing quiz details.
- *                    - Returns null if quiz is not found or user is not authorized.
- *                    - Example return object:
- *                      {
- *                          quizId: 1,
- *                          name: 'My Quiz',
- *                          timeCreated: 1683125870,
- *                          timeLastEdited: 1683125871,
- *                          description: 'This is my quiz',
- *                      }
+ * @returns {quizId: number, name: string, timeCreated: number, timeLastEdited: number, description: string}
  * 
  */
 function adminQuizInfo(authUserId, quizId ) {
