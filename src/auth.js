268962085c1d7da5c584550931decc4229bf72f1
Abrar Gofur <z5479434@ad.unsw.edu.au>
2024-02-21 14:17:45 +1100

    Adding Stub Code for adminUserDetails

--- src/auth.js
+++ src/auth.js
@@ -6,7 +6,10 @@
   * @param {string} newPassword - new password of admin user
   * @returns { } - empty object
 */
-function adminAuthLogin( email, password ) {
-  
-  return { authUserId: 1 }
+function adminUserDetails( authUserId ) {
+  return { user: { userId: 1,
+     name: 'Hayden Smith',
+      email: 'hayden.smith@unsw.edu.au',
+   numSuccessfulLogins: 3,
+    numFailedPasswordsSinceLastLogin: 1, } }
 }
