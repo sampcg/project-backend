066bcc9ed42db19a6bd2bfd3c8239409cafedcea
Sam <z5363931@ad.unsw.edu.au>
2024-02-21 16:56:02 +1100

    Fixing formating and removing lines

--- src/quiz.js
+++ src/quiz.js
@@ -1,13 +1,12 @@
 // 
 /**
- *
+ * Updates the name of the relevant quiz.
  * @param {number} authUserId - unique identifier for an authorated user
  * @param {number} quizId - unique identifier for quiz 
-* @param {string} name - updated name for relevant quiz
+ * @param {string} name - updated name for relevant quiz
  * @returns {} an empty object
 */
 
 function adminQuizNameUpdate( authUserId, quizId, name ) {
-	return {
-	};
+	return {};
 }
