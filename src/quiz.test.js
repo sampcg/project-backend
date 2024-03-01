efcbd46f9471097e9a8e879c78cd7827df7e66d4
Michael Hourn <z5419452@ad.unsw.edu.au>
2024-03-01 15:42:41 +1100

    Fixed quiz.test.js to include clear and remove import of data from dataStore.js

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -1,10 +1,14 @@
 import { adminAuthRegister } from './auth.js';
+import { clear } from './other.js';
 import { adminQuizCreate } from './quiz.js';
-import { data } from './dataStore.js';
 
 // adminQuizCreate Testing
 
 describe('adminQuizCreate', () => {
+
+    beforeEach(() => {
+        clear();
+    })
 
     test('Invalid Author User ID', () => {
         const authEmail = 'aaa@bbb.com';
@@ -24,11 +28,11 @@ describe('adminQuizCreate', () => {
         const authPassword = 'abcde12345';
         const authNameFirst = 'Michael';
         const authNameLast = 'Hourn';
-        adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
+        let authId = adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
         
         const invalidQuizName = '-----';
         const quizDescription = 'Quiz Description';
-        expect(adminQuizCreate(data.users[0].userId, invalidQuizName, quizDescription)).toMatchObject({error: expect.any(String)});
+        expect(adminQuizCreate(authId, invalidQuizName, quizDescription)).toMatchObject({error: expect.any(String)});
     });
 
     test('Name is less than 3 characters long', () => {
@@ -36,11 +40,11 @@ describe('adminQuizCreate', () => {
         const authPassword = 'abcde12345';
         const authNameFirst = 'Michael';
         const authNameLast = 'Hourn';
-        adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
+        let authId = adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
 
         const shortQuizName = 'a';
         const quizDescription = 'Quiz Description';
-        expect(adminQuizCreate(users[0].userId, 'a', 'Quiz Description')).toMatchObject({error: expect.any(String)});
+        expect(adminQuizCreate(authId, 'a', 'Quiz Description')).toMatchObject({error: expect.any(String)});
     });
 
     test('Name is more than 30 characters long', () => {
@@ -48,11 +52,11 @@ describe('adminQuizCreate', () => {
         const authPassword = 'abcde12345';
         const authNameFirst = 'Michael';
         const authNameLast = 'Hourn';
-        adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
+        let authId = adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
 
         const longName = '123456789 123456789 123456789 123456789';
         const quizDescription = 'Quiz Description';
-        expect(adminQuizCreate(data.users[0].userId, longName, quizDescription)).toMatchObject({error: expect.any(String)});
+        expect(adminQuizCreate(authId, longName, quizDescription)).toMatchObject({error: expect.any(String)});
     });
 
     test('Name is already used by another quiz', () => {
@@ -60,20 +64,20 @@ describe('adminQuizCreate', () => {
         const auth1Password = 'abcde12345';
         const auth1NameFirst = 'Michael';
         const auth1NameLast = 'Hourn';
-        adminAuthRegister(auth1Email, auth1Password, auth1NameFirst, auth1NameLast);
+        let auth1Id = adminAuthRegister(auth1Email, auth1Password, auth1NameFirst, auth1NameLast);
 
         const auth2Email = 'ccc@ddd.com';
         const auth2Password = '12345abcde';
         const auth2NameFirst = 'John';
         const auth2NameLast = 'Doe';
-        adminAuthRegister(auth2Email, auth2Password, auth2NameFirst, auth2NameLast);
+        let auth2Id = adminAuthRegister(auth2Email, auth2Password, auth2NameFirst, auth2NameLast);
         
         const quizName = 'Quiz Name';
         const quizDescription = 'Quiz Description';
 
-        adminQuizCreate(users[0].userId, quizName, quizDescription);
+        adminQuizCreate(auth1Id, quizName, quizDescription);
 
-        expect(adminQuizCreate(data.users[1].userId, quizName, quizDescription)).toMatchObject({error: expect.any(String)});        
+        expect(adminQuizCreate(auth2Id, quizName, quizDescription)).toMatchObject({error: expect.any(String)});        
     });
 
     test('Description is more than 100 characters', () => {
@@ -81,11 +85,11 @@ describe('adminQuizCreate', () => {
         const authPassword = 'abcde12345';
         const authNameFirst = 'Michael';
         const authNameLast = 'Hourn';
-        adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
+        let authId = adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
 
         const quizName = 'Quiz Name'
         const longDescription = '123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789';
-        expect(adminQuizCreate(data.users[0].userId, quizName, longDescription)).toMatchObject({error: expect.any(String)});
+        expect(adminQuizCreate(authId, quizName, longDescription)).toMatchObject({error: expect.any(String)});
     });
 
     test('adminQuizCreate has correct return type', () => {
@@ -93,11 +97,11 @@ describe('adminQuizCreate', () => {
         const authPassword = 'abcde12345';
         const authNameFirst = 'Michael';
         const authNameLast = 'Hourn';
-        adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
+        let authId = adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
 
         const quizName = 'Quiz Name';
         const quizDescription = 'Quiz Description';
-        expect(adminQuizCreate(users[0].userId, quizName, quizDescription)).toStrictEqual({quizId: expect.any(Number)});
+        expect(adminQuizCreate(authId, quizName, quizDescription)).toStrictEqual({quizId: expect.any(Number)});
     });
 
     test('adminQuizCreate works with empty description', () => {
@@ -105,11 +109,11 @@ describe('adminQuizCreate', () => {
         const authPassword = 'abcde12345';
         const authNameFirst = 'Michael';
         const authNameLast = 'Hourn';
-        adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
+        let authId = adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
 
         const quizName = 'Quiz Name';
         const quizDescription = '';
-        expect(adminQuizCreate(data.users[0].userId, quizName, quizDescription)).toStrictEqual({quizId: expect.any(Number)});
+        expect(adminQuizCreate(authId, quizName, quizDescription)).toStrictEqual({quizId: expect.any(Number)});
     });
 
     test('If added to database', () => {
@@ -117,12 +121,12 @@ describe('adminQuizCreate', () => {
         const authPassword = 'abcde12345';
         const authNameFirst = 'Michael';
         const authNameLast = 'Hourn';
-        adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
+        let authId = adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
 
         const quizName = 'Quiz Name';
         const quizDescription = 'Quiz Description';
         
-        const quiz = adminQuizCreate(users[0].userId, quizName, quizDescription);
+        const quiz = adminQuizCreate(authId, quizName, quizDescription);
         expect(data()).toStrictEqual({
             quizzes: [{quizId: quiz.quizId, name: quizName, description: quizDescription}]
         });
@@ -133,20 +137,36 @@ describe('adminQuizCreate', () => {
         const auth1Password = 'abcde12345';
         const auth1NameFirst = 'Michael';
         const auth1NameLast = 'Hourn';
-        adminAuthRegister(auth1Email, auth1Password, auth1NameFirst, auth1NameLast);
+        let auth1Id = adminAuthRegister(auth1Email, auth1Password, auth1NameFirst, auth1NameLast);
 
         const auth2Email = 'ccc@ddd.com';
         const auth2Password = '12345abcde';
         const auth2NameFirst = 'John';
         const auth2NameLast = 'Doe';
-        adminAuthRegister(auth2Email, auth2Password, auth2NameFirst, auth2NameLast);
+        let auth2Id = adminAuthRegister(auth2Email, auth2Password, auth2NameFirst, auth2NameLast);
 
         const quiz1Name = 'Quiz 1';
         const quiz2Name = 'Quiz 2';
         const quiz1Description = 'Quiz 1 description';
         const quiz2Description = 'Quiz 2 description';
 
-        const quiz1 = adminQuizCreate(users[0].userId, quiz1Name, quiz1Description);
-        const quiz2 = adminQuizCreate(users[0].userId, quiz2Name, quiz2Description);
+        const quiz1 = adminQuizCreate(auth1Id, quiz1Name, quiz1Description);
+        const quiz2 = adminQuizCreate(auth2Id, quiz2Name, quiz2Description);
+
+        expect(data().toStrictEqual({
+            quizzes: [
+                {
+                    quizId: quiz1.quizId,
+                    name: quiz1Name,
+                    description: quiz1Description,
+
+                },
+                {
+                    quizId: quiz2.quizId,
+                    name: quiz2Name,
+                    description: quiz2Description,
+                }
+            ]
+        }))
     });
 });
