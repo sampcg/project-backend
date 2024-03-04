5649cc1d4ed36a24ea0a587330662ceac0433e02
Sam <z5363931@ad.unsw.edu.au>
2024-03-04 21:21:47 +1100

    Updated to take in quizId and added imports

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -1,7 +1,7 @@
 import { adminQuizInfo } from './quiz.js';
 import { adminQuizCreate } from './quiz.js';
 import { adminAuthRegister } from './auth.js';
-import { clear } from './other.js'
+import { clear } from './other.js';
 
 // adminQuizInfo Testing
 
@@ -9,22 +9,72 @@ import { clear } from './other.js'
 // Quiz ID does not refer to a valid quiz.
 // Quiz ID does not refer to a quiz that this user owns. 
 
+// a
+
+beforeEach(() => {
+    clear();
+})
+
 describe('adminQuizInfo', () =>  {
+    let authUserId, quizId;
 
     beforeEach(() => {
-        clear();
-    })
-
-    test('Invalid Author User ID', () => {
         const authEmail = 'aaa@bbb.com';
         const authPassword = 'abcde12345';
         const authNameFirst = 'Samuel';
         const authNameLast = 'Gray';
-        adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
+        const user = adminAuthRegister(authEmail, authPassword, authNameFirst, authNameLast);
+        authUserId = user.authUserId;
         
-        const invalidAuthId = 2;
-        const quizName = 'Quiz Name';
+        const quizName = 'Test Quiz';
         const quizDescription = 'Quiz Description';
-        expect(adminQuizCreate(invalidAuthId, quizName, quizDescription)).toMatchObject({error: expect.any(String)});
+        const quiz = adminQuizCreate(authUserId, quizName, quizDescription);
+        quizId = quiz.quizId;
+    });
+
+    test('Returns information about quiz when provided with valid authUserId', () => {
+        const expectedInfo = {
+            quizId: quizId,
+            name: 'Test Quiz',
+            timeCreated: expect.any(Date), 
+            timeLastEdited: expect.any(Date), 
+            description: 'This is a test quiz.'
+        };
+        const result = adminQuizInfo(authUserId, quizId);
+        expect(result).toEqual(expectedInfo);
+    });
+
+    test('Returns error message when authUserId is not a valid user', () => {
+        const invalidUserId = authUserId + 1;
+        const expectedError = { error: 'AuthUserId is not a valid user.' };
+        const result = adminQuizInfo(invalidUserId, quizId);
+        expect(result).toEqual(expectedError);
+    });
+
+    test('Returns error message when quizId does not refer to a valid quiz', () => {
+        const invalidQuizId = quizId + 1;
+        const expectedError = { error: 'Quiz ID does not refer to a valid quiz.' };
+        const result = adminQuizInfo(authUserId, invalidQuizId);
+        expect(result).toEqual(expectedError);
+    });
+
+    test('Returns error message when quizId does not refer to a quiz that this user owns', () => {
+        const anotherUserId = authUserId + 1;
+        const expectedError = { error: 'Quiz ID does not refer to a quiz that this user owns.' };
+        const result = adminQuizInfo(anotherUserId, quizId);
+        expect(result).toEqual(expectedError);
+    });
+
+    test('Return type if no error', () => {
+        const result = adminQuizInfo(authUserId, quizId);
+        expect(result).toEqual({
+            quizId: expect.any(Number),
+            name: expect.any(String),
+            timeCreated: expect.any(Date),
+            timeLastEdited: expect.any(Date),
+            description: expect.any(String)
+        });
     });
 });
+
+
