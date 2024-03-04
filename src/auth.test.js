f1753fd1af1225fac540e52467b7fc74c81f6af6
Abrar Gofur <z5479434@ad.unsw.edu.au>
2024-03-04 22:40:08 +1100

    More Test Cases for AuthRegister

--- src/auth.test.js
+++ src/auth.test.js
@@ -23,36 +23,70 @@ describe('adminAuthRegister', () => {
 
 //1)Checking for a successful creation 
     test('Checking if successful and already used email', () =>{
+        clear();
 
         //Inserting a parameters and expect a successful number
         expect(adminAuthRegister(authEmail, authPassword, authNameFirst,
              authNameLast)).toStrictEqual({ authUserId: expect.any(Number)} );
         
-        expect(adminAuthRegister(authEmail, authPassword, authNameFirst,
-            authNameLast)).toStrictEqual({ error: expect.any(String)} );
+        expect(adminAuthRegister(authEmail, authPassword, Abrar,
+            Gofur)).toStrictEqual({ error: expect.any(String)} );
            
 
     })
 
 //2)Checking for valid email structure
     test('Checking for valid emails', () => {
+        clear();
 
+        expect(adminAuthRegister(authEmail, authPassword, authNameFirst,
+            authNameLast)).toStrictEqual({ authUserId: expect.any(Number)} );
 
+        expect(adminAuthRegister('12342132', authPassword, authNameFirst,
+             authNameLast)).toStrictEqual({ error: expect.any(String)} );
 
+        expect(adminAuthRegister('Hello,World!', authPassword, authNameFirst,
+             authNameLast)).toStrictEqual({ error: expect.any(String)} );
+
+        expect(adminAuthRegister('@.com', authPassword, authNameFirst,
+             authNameLast)).toStrictEqual({ error: expect.any(String)} );
+
+        expect(adminAuthRegister('.com.@', authPassword, authNameFirst,
+             authNameLast)).toStrictEqual({ error: expect.any(String)} );
 
     })
 
 //3) NameFirst contains characters other than lowercase letters, uppercase letters, spaces, hyphens, or apostrophes
 test('Checking for invalid characters firstName', () => {
+    clear();
 
+    expect(adminAuthRegister(authEmail, authPassword, authNameFirst,
+        authNameLast)).toStrictEqual({ authUserId: expect.any(Number)} );
 
+    expect(adminAuthRegister(authEmail, authPassword, 'Abrar!',
+        authNameLast)).toStrictEqual({ error: expect.any(String)} );
+
+    expect(adminAuthRegister(authEmail, authPassword, 'Abrar#',
+        authNameLast)).toStrictEqual({ error: expect.any(String)} );
+
+    expect(adminAuthRegister(authEmail, authPassword, 'Sam(parameters)',
+        authNameLast)).toStrictEqual({ error: expect.any(String)} );
+
+    expect(adminAuthRegister(authEmail, authPassword, 'Miche@l',
+        authNameLast)).toStrictEqual({ error: expect.any(String)} );
+
+    expect(adminAuthRegister(authEmail, authPassword, 'Miche*l',
+        authNameLast)).toStrictEqual({ error: expect.any(String)} );
+
+    expect(adminAuthRegister(authEmail, authPassword, '9+10=21',
+        authNameLast)).toStrictEqual({ error: expect.any(String)} );
 
 
 })
 
 //4)NameFirst is less than 2 characters or more than 20 characters
 test('Checking length of firstName', () => {
-
+    clear();
 
 
 
@@ -60,15 +94,35 @@ test('Checking length of firstName', () => {
 
 //5)NameLast contains characters other than lowercase letters, uppercase letters, spaces, hyphens, or apostrophes.
 test('Checking for invalid character lastName', () => {
+    clear();
 
+    expect(adminAuthRegister(authEmail, authPassword, authNameFirst,
+        authNameLast)).toStrictEqual({ authUserId: expect.any(Number)} );
 
+    expect(adminAuthRegister(authEmail, authPassword, authNameFirst,
+    'Abrar!')).toStrictEqual({ error: expect.any(String)} );
+
+    expect(adminAuthRegister(authEmail, authPassword, authNameFirst,
+    'Abrar#')).toStrictEqual({ error: expect.any(String)} );
+
+    expect(adminAuthRegister(authEmail, authPassword, authNameFirst,
+    'Sam(parameters)')).toStrictEqual({ error: expect.any(String)} );
+
+    expect(adminAuthRegister(authEmail, authPassword, authNameFirst,
+    'Miche@l')).toStrictEqual({ error: expect.any(String)} );
+
+    expect(adminAuthRegister(authEmail, authPassword, authNameFirst,
+    'Miche*l')).toStrictEqual({ error: expect.any(String)} );
+
+    expect(adminAuthRegister(authEmail, authPassword, authNameFirst,
+    '9+10=21')).toStrictEqual({ error: expect.any(String)} );
 
 
 })
 
 //6)NameLast is less than 2 characters or more than 20 characters.
 test('Checking length of lastName', () => {
-
+    clear();
 
 
 
@@ -76,12 +130,12 @@ test('Checking length of lastName', () => {
 
 //8)Password is less than 8 characters.
 test('Checking Password length, if , < 8' , () => {
-
+    clear();
 });
 
 //9)Password does not contain at least one number and at least one letter
     test('Checking Password 1 number and 1 letter, if , < 8' , () => {
-
+    clear();
 });
 
 });
