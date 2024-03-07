32ebd277191c12fd37c0e33a97e28266aa583956
Sam <z5363931@ad.unsw.edu.au>
2024-03-07 15:42:57 +1100

    Finished implementing and testing adminQuizNameUpdate

--- src/quiz.js
+++ src/quiz.js
@@ -1,171 +1,5 @@
-import { getData, setData } from './dataStore.js'
-
-/** 
- * Provides a list of all quizzed owned by the currently logged in user
- * @param {number} authUserId - unique identifier for the user
- * @returns {quizzes: {quizId: number, name: string}} - information on quizzes
- */
-
-export function adminQuizList(authUserId) {
-    let data = getData();
-
-    // Checks if userId is valid
-    const userExists = data.users.some(user => user.userId === authUserId);
-    if (!userExists) {
-        return { error: 'Invalid user ID' }
-    }
-
-    // Creates array of quizzes to return
-    const quizzes = [];
-
-    // Pushes all quizzes with given user ID in data to array quizzes 
-    for (let quiz of data.quizzes) {
-        if (quiz.userId === authUserId) {
-            quizzes.push({quizId: quiz.quizId, name: quiz.name});
-        }
-    }
-
-    // Returns object containing array of all quizzes owned by user
-    return {quizzes: quizzes };
-}
-
-/** 
- * Creates a quiz for the logged in user given basic details
- * @param {number} authUserId - unique identifier for admin user
- * @param {string} name - name for the quiz
- * @param {string} description - description of the quiz
- * @returns {{quizId: number}} - quizId
- */
-
-export function adminQuizCreate(authUserId, name, description) {
-    let data = getData();
-
-    // Check if user is valid
-    const userExists = data.users.some(user => user.userId === authUserId);
-    if (!userExists) {
-        return { error: 'Invalid user ID' }
-    }
-    
-    // Check if name contains invalid characters
-    const validName = /^[a-zA-Z0-9\s]*$/.test(name);
-    if (!validName) {
-        return { error: 'Name contains invalid characters' }
-    }
-
-    // Check if name is less than 3 characters or greater than 30
-    if (name.length < 3 || name.length > 30) {
-        return { error: 'Name must be between 3 and 30 characters' };
-    }
-
-    // Check if name there is already a quiz by that name 
-    // makes sure case doesn't impact check
-    const nameExists = data.quizzes.some(quiz => quiz.name.toLowerCase() === name.toLowerCase());
-    if (nameExists) {
-        return { error: 'invalid input' };
-    }
-
-    // Check the length of the description
-    if (description.length > 100) {
-        return { error: 'Description must be 100 characters or less' };
-    }
-
-    // Generate new quiz ID
-    const newQuizId = data.quizzes.length + 1;
-
-    // Create parameters for new quiz
-    const newQuiz = {
-        quizId: newQuizId,
-        name: name,
-        description: description,
-        timeCreated: Date.now()/1000,
-        timeLastEdited: Date.now()/1000,
-        userId: authUserId
-    };
-
-    // Add quiz to data
-    data.quizzes.push(newQuiz);
-
-    // Return quizId
-    return {
-        quizId: newQuizId
-    };
-}
-
-/**
- * Removes a quiz given author and quiz IDs
- * @param {number} authUserId - unique identifer for the user
- * @param {number} quizId - unique identifier for quiz
- * @returns { } - empty object
- */
-
-export function adminQuizRemove(authUserId, quizId) {
-    let data = getData();
-
-    // Check if user is valid
-    const userExists = data.users.some(user => user.userId === authUserId);
-    if (!userExists) {
-        return { error: 'Invalid user ID' }
-    }
-
-    // Check if quizId is valid
-    const quizExists = data.quizzes.some(quiz => quiz.quizId === quizId);
-    if (!quizExists) {
-        return { error: 'Invalid quiz ID' };
-    }
-
-    // Check if owner owns quiz
-    const findQuiz = data.quizzes.find(quiz => quiz.quizId === quizId);
-    if (findQuiz.userId !== authUserId) {
-        return { error: 'User does not own this quiz' };
-    }
-
-    // Remove quiz that has given quizId
-    data.quizzes = data.quizzes.filter(quiz => quiz.quizId !== quizId);
-
-    // Return empty object
-    return {};
-}
-
-/**
- * Updates the description of the relevant quiz
- * @param {number} authUserId - unique identifier for an authorated user
- * @param {number} quizId - unique identifier for quiz 
- * @param {string} description - updated description for relevant quiz
- * @returns {} an empty object
- */
-
-// Update the description of the relevant quiz.
-function adminQuizDescriptionUpdate( authUserId, quizId, description ) {
-    return {};
-}
-
-/**
- * Program to get all of the relevant information about the current quiz
- * @param {number} authUserId - unique identifier for an authorated user
- * @param {number} quizId - unique identifier for quiz 
- * @returns {{academic: {name: string, hobby: string}}}
- * @returns {quizId: number, name: string, timeCreated: number, timeLastEdited: number, description: string}
- */
-
-function adminQuizInfo(authUserId, quizId ) {
-   return { 
-        quizId: 1, 
-        name: 'My Quiz', 
-        timeCreated: 1683125870,
-        timeLastEdited: 1683125871, 
-        description: 'This is my quiz', 
-    };
-}
-
-/**
- * Updates the name of the relevant quiz.
- * @param {number} authUserId - unique identifier for an authorated user
- * @param {number} quizId - unique identifier for quiz 
- * @param {string} name - updated name for relevant quiz
- */
-
-
 import { getData } from './dataStore.js';
+import { setData } from './dataStore.js';
 
 function adminQuizNameUpdate(authUserId, quizId, name) {
     const data = getData();
@@ -189,7 +23,7 @@ function adminQuizNameUpdate(authUserId, quizId, name) {
 
     // Validate the name
     if (!isValidName(name)) {
-        return { error: 'Name contains invalid characters. Valid characters are alphanumeric and spaces.' };
+        return { error: 'Name contains invalid characters.' };
     }
 
     if (name.length < 3 || name.length > 30) {
@@ -202,10 +36,10 @@ function adminQuizNameUpdate(authUserId, quizId, name) {
         return { error: 'Name is already used by the current logged in user for another quiz.' };
     }
 
-    // Update the name of the quiz
     quiz.name = name;
+    data.quizzes[quizId].timeLastEdited = Date.now()/1000
 
-    // Return empty object indicating success
+
     return {};
 }
 
@@ -222,6 +56,7 @@ function isValidName(name) {
     }
     return true;
     }
-export default adminQuizNameUpdate;
+
+export { adminQuizNameUpdate };
 
 
