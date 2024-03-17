5537acab14fe6dcb2e2608090ae9eacf6277e65d
Zechen Zhu <z5389200@ad.unsw.edu.au>
2024-03-18 02:42:13 +1100

    fix  linting errors in newecho.ts, other.test.js, quiz.js and quiz.test.js

--- src/other.test.js
+++ src/other.test.js
@@ -1,14 +1,19 @@
-import { clear } from "./other.js";
-import { getData } from "./dataStore.js";
-import { adminAuthRegister } from "./auth.js";
-import { adminQuizCreate } from "./quiz.js";
+import { clear } from './other.js';
+import { adminAuthRegister, adminUserDetails } from './auth.js';
+import { adminQuizCreate, adminQuizList } from './quiz.js';
 
-/*---------------------- Clear function tests -------------------*/
-describe("clear function tests", () => {
+const ERROR = { error: expect.any(String) };
+
+/* ---------------------- Clear function tests ------------------- */
+describe('clear function tests', () => {
   test('Test clearing data', () => {
-    const data = getData();
+    expect(clear()).toStrictEqual({});
     const testUserId = adminAuthRegister('hayden.smith@unsw.edu.au', '123456ABC', 'Hayden', 'Smith');
     const testQuizId = adminQuizCreate(testUserId.authUserId, 'My Quiz', 'This is my quiz');
-    expect(clear()).toStrictEqual({});
+    expect(adminUserDetails(testUserId.authUserId)).toStrictEqual(expect.any(Object));
+    expect(adminQuizList(testQuizId.authUserId)).toStrictEqual(expect.any(Object));
+    clear();
+    expect(adminUserDetails(testUserId.authUserId)).toStrictEqual(ERROR);
+    expect(adminQuizList(testQuizId.authUserId)).toStrictEqual(ERROR);
   });
 });
--- src/quiz.js
+++ src/quiz.js
@@ -1,25 +1,25 @@
-import { getData, setData } from './dataStore.js'
+import { getData, setData } from './dataStore.js';
 
-/** 
+/**
  * Provides a list of all quizzed owned by the currently logged in user
  * @param {number} authUserId - unique identifier for the user
  * @returns {quizzes: {quizId: number, name: string}} - information on quizzes
  */
 
 export function adminQuizList(authUserId) {
-  let data = getData();
+  const data = getData();
 
   // Checks if userId is valid
   const userExists = data.users.some(user => user.userId === authUserId);
   if (!userExists) {
-    return { error: 'Invalid user ID' }
+    return { error: 'Invalid user ID' };
   }
 
   // Creates array of quizzes to return
   const quizzes = [];
 
-  // Pushes all quizzes with given user ID in data to array quizzes 
-  for (let quiz of data.quizzes) {
+  // Pushes all quizzes with given user ID in data to array quizzes
+  for (const quiz of data.quizzes) {
     if (quiz.userId === authUserId) {
       quizzes.push({ quizId: quiz.quizId, name: quiz.name });
     }
@@ -29,7 +29,7 @@ export function adminQuizList(authUserId) {
   return { quizzes: quizzes };
 }
 
-/** 
+/**
  * Creates a quiz for the logged in user given basic details
  * @param {number} authUserId - unique identifier for admin user
  * @param {string} name - name for the quiz
@@ -38,18 +38,18 @@ export function adminQuizList(authUserId) {
  */
 
 export function adminQuizCreate(authUserId, name, description) {
-  let data = getData();
+  const data = getData();
 
   // Check if user is valid
   const userExists = data.users.some(user => user.userId === authUserId);
   if (!userExists) {
-    return { error: 'Invalid user ID' }
+    return { error: 'Invalid user ID' };
   }
 
   // Check if name contains invalid characters
   const validName = /^[a-zA-Z0-9\s]*$/.test(name);
   if (!validName) {
-    return { error: 'Name contains invalid characters' }
+    return { error: 'Name contains invalid characters' };
   }
 
   // Check if name is less than 3 characters or greater than 30
@@ -57,7 +57,7 @@ export function adminQuizCreate(authUserId, name, description) {
     return { error: 'Name must be between 3 and 30 characters' };
   }
 
-  // Check if name there is already a quiz by that name 
+  // Check if name there is already a quiz by that name
   // makes sure case doesn't impact check
   const nameExists = data.quizzes.some(quiz => quiz.name.toLowerCase() === name.toLowerCase());
   if (nameExists) {
@@ -99,12 +99,12 @@ export function adminQuizCreate(authUserId, name, description) {
  */
 
 export function adminQuizRemove(authUserId, quizId) {
-  let data = getData();
+  const data = getData();
 
   // Check if user is valid
   const userExists = data.users.some(user => user.userId === authUserId);
   if (!userExists) {
-    return { error: 'Invalid user ID' }
+    return { error: 'Invalid user ID' };
   }
 
   // Check if quizId is valid
@@ -129,7 +129,7 @@ export function adminQuizRemove(authUserId, quizId) {
 /**
  * Updates the name of the relevant quiz
  * @param {number} authUserId - unique identifier for an authorated user
- * @param {number} quizId - unique identifier for quiz 
+ * @param {number} quizId - unique identifier for quiz
  * @param {string} name - updated name for relevant quiz
  * @returns {} an empty object
  */
@@ -156,7 +156,7 @@ export function adminQuizNameUpdate(authUserId, quizId, name) {
   // Validate the name
   const validName = /^[a-zA-Z0-9\s]*$/.test(name);
   if (!validName) {
-    return { error: 'Name contains invalid characters' }
+    return { error: 'Name contains invalid characters' };
   }
 
   if (name.length < 3 || name.length > 30) {
@@ -174,18 +174,17 @@ export function adminQuizNameUpdate(authUserId, quizId, name) {
   return {};
 }
 
-
 /**
  * Updates the description of the relevant quiz
  * @param {number} authUserId - unique identifier for an authorated user
- * @param {number} quizId - unique identifier for quiz 
+ * @param {number} quizId - unique identifier for quiz
  * @param {string} description - updated name for relevant quiz
  * @returns {} an empty object
  */
 // Update the description of the relevant quiz.
 
 export function adminQuizDescriptionUpdate(authUserId, quizId, description) {
-  let data = getData();
+  const data = getData();
 
   // Check if user is valid
   const user = data.users.find(user => user.userId === authUserId);
@@ -222,12 +221,11 @@ export function adminQuizDescriptionUpdate(authUserId, quizId, description) {
 /**
  * Program to get all of the relevant information about the current quiz
  * @param {number} authUserId - unique identifier for an authorated user
- * @param {number} quizId - unique identifier for quiz 
+ * @param {number} quizId - unique identifier for quiz
  * @returns {quizId: number, name: string, timeCreated: number, timeLastEdited: number, description: string}
  */
 
 export function adminQuizInfo(authUserId, quizId) {
-
   const data = getData();
 
   const user = data.users.find(user => user.userId === authUserId);
@@ -238,11 +236,11 @@ export function adminQuizInfo(authUserId, quizId) {
 
   const quiz = data.quizzes.find(quiz => quiz.quizId === quizId);
   if (!quiz) {
-    return { error: 'Quiz ID does not refer to a valid quiz.' }
+    return { error: 'Quiz ID does not refer to a valid quiz.' };
   }
 
   if (quiz.userId !== authUserId) {
-    return { error: 'Quiz ID does not refer to a quiz that this user owns.' }
+    return { error: 'Quiz ID does not refer to a quiz that this user owns.' };
   }
 
   return {
@@ -251,6 +249,5 @@ export function adminQuizInfo(authUserId, quizId) {
     timeCreated: quiz.timeCreated,
     timeLastEdited: Date.now() / 1000,
     description: quiz.description
-  }
-
+  };
 }
