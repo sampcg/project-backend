bda9f0a152dc367a2fff296bcc555c03b9761b33
Abrar Gofur <z5479434@ad.unsw.edu.au>
2024-03-21 22:57:34 +1100

    Writing my HTTP Tests for AuthRegister

--- src/server.test.ts
+++ src/server.test.ts
@@ -9,8 +9,83 @@ import { Trash } from '/returnInterfaces';
 const SERVER_URL = `${url}:${port}`;
 
 beforeEach(() => {
-    request('DELETE', `${SERVER_URL}/clear`);
+    request('DELETE', `${SERVER_URL}/v1/clear`);
   });
   
+  //  BEGINNING OF TESTING ADMIN AUTH REGISTER  //
+  describe('Testing POST /v1/admin/auth/register', () => {
 
+    //This is the correct output for AdminAuthRegister
+    test('Correct status code and return value', () => {
+      const AuthRegisterResponse = request('POST', `${SERVER_URL}/v1/admin/auth/register`, {
+        json: {
+          email: 'aaa@bbb.com', 
+          password: 'abcde12345',
+          nameFirst: 'Michael',
+          nameLast:'Hourn'
+        }
+      });
+
+      expect(AuthRegisterResponse.statusCode).toStrictEqual(200);
+
+      const AuthRegisterResponse = JSON.parse(AuthRegisterResponse.body.toString());
+      expect(AuthRegisterResponse).toStrictEqual({ authUserId: expect.any(Number) });
+    });
+
+    //These are all the error cases
+    test.each([
+      {
+        //Already used email
+        email: 'aaa@bbb.com', 
+        password: 'abcde12345',
+        nameFirst: 'Abrar',
+        nameLast:'Gofur'
+      }, 
+      {
+        email: '12342132', 
+        password: 'abcde12345',
+        nameFirst: 'Abrar',
+        nameLast:'Gofur'
+      },
+      {
+        email: '@.com', 
+        password: 'abcde12345',
+        nameFirst: 'Abrar',
+        nameLast:'Gofur'
+      },
+      {
+        email: 'Hello,World!', 
+        password: 'abcde12345',
+        nameFirst: 'Abrar',
+        nameLast:'Gofur'
+      },
+      {   
+        email: '.com.@', 
+        password: 'abcde12345',
+        nameFirst: 'Abrar',
+        nameLast:'Gofur'
+      },
+    ]) (
+      'Checking for valid emails and already used or not',
+      ({ email, password, nameFirst, nameLast }) => {
+        const AuthRegisterResponse = request('POST', `${SERVER_URL}/v1/admin/auth/register`, {
+          json: {
+            email, password, nameFirst, nameLast}
+        });
+        expect(AuthRegisterResponse.statusCode).toStrictEqual(400);
+        const AuthRegisterResponse = JSON.parse(AuthRegisterResponse.body.toString());
+        expect(AuthRegisterResponse).toStrictEqual({ error: expect.any(String) });
+  
+      });
+
+    test.each([
+
+      
+
+
+    ])
+
+
+  });
+  
   
