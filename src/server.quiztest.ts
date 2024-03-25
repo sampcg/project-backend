23567d344027c87a9e2aa8a83af83b0e2cd023a1
Michael Hourn <z5419452@ad.unsw.edu.au>
2024-03-25 16:30:00 +1100

    Added tests for creating question

--- src/server.quiztest.ts
+++ src/server.quiztest.ts
@@ -6,6 +6,16 @@ const SERVER_URL = `${url}:${port}`;
 
 const makeCustomErrorForTest = (status: number) => ({ status, error: expect.any(String) });
 
+interface Answer {
+    answer: string;
+    correct: boolean;
+};
+
+interface QuestionBody {
+    
+};
+
+
 const requestHelper = (method: HttpVerb, path: string, payload: object) => {
     let qs = {};
     let json= {};
@@ -40,24 +50,50 @@ const requestAuthLogin = (email: string, password: string) => {
     return requestHelper('POST', '/v1/admin/auth/login', { email, password });
 };
 
-const requestAuthLogout = (token: number) => {
+const requestAuthLogout = (token: string) => {
     return requestHelper('POST', '/v1/admin/auth/logout', { token });
 };
 
-const requestQuizCreate = (token: number, name: string, description: string) => {
+const requestQuizCreate = (token: string, name: string, description: string) => {
     return requestHelper('POST', '/v1/admin/quiz', { token, name, description });
 };
 
-const requestQuizList = (token: number) => {
+const requestQuizList = (token: string) => {
     return requestHelper('GET', '/v1/admin/quiz/list', { token });
 };
 
-const requestQuizRemove = (token: number, quizId: number) => {
-    return requestHelper('DELETE', `/v1/admin/quiz/${quizId}`, { token, quizId })
+const requestQuizRemove = (token: string, quizId: number) => {
+    return requestHelper('DELETE', `/v1/admin/quiz/${quizId}`, { token, quizId });
 };
 
-const requestTrashList = (token: number) => {
+const requestQuizInfo = (token: string, quizId: number) => {
+    return requestHelper('GET', `/v1/admin/quiz/${quizId}`, { token, quizId });
+};
+
+const requestTrashList = (token: string) => {
     return requestHelper('GET', '/v1/admin/quiz/trash', { token });
+};
+
+const requestQuestionCreate = (
+    token: string,
+    quizId: number,
+    questionBody: {
+        question: string,
+        duration: number,
+        points: number,
+        answers: Answer[]
+        }
+    ) => {
+    return requestHelper('PUT', `/v1/admin/quiz/${quizId}/question`, {
+        token,
+        quizId,
+        questionBody: {},
+        }
+    );
+};
+
+const requestQuestionDelete = (token: string, quizId: number, questionId: number) => {
+    return requestHelper('DELETE', `/v1/admin/quiz/${quizId}/question/${questionId}`, { token, quizId, questionId });
 };
 
 const requestClear = () => {
@@ -74,7 +110,7 @@ beforeEach(() => {
 ////////////////////       Testing for Listing Quizzes      ////////////////////
 
 describe('Testing GET /v1/admin/quiz/list', () => {
-    let author: {token: number} ;
+    let author: {token: string} ;
     beforeEach(() => {
         author = requestRegisterAuth('aaa@bbb.com', 'abcde12345', 'Michael', 'Hourn');
         requestAuthLogin('aaa@bbb.com', 'abcde12345');
@@ -128,7 +164,7 @@ describe('Testing GET /v1/admin/quiz/list', () => {
             const quiz1: {quizId: number} = requestQuizCreate(author.token, 'Quiz 1', 'a');
             requestAuthLogout(author.token);
 
-            const author2: {token: number} = requestRegisterAuth('ccc@ddd.com', '12345abcde', 'John', 'Doe');
+            const author2: {token: string} = requestRegisterAuth('ccc@ddd.com', '12345abcde', 'John', 'Doe');
             requestAuthLogin('ccc@ddd.com', '12345abcde');
             
             const quiz2: {quizId: number} = requestQuizCreate(author.token, 'Quiz 2', 'b');
@@ -159,7 +195,7 @@ describe('Testing GET /v1/admin/quiz/list', () => {
 ////////////////////        Testing for Creating Quiz       ////////////////////
 
 describe('Testing POST /v1/admin/quiz', () => {
-    let author: {token: number} ;
+    let author: {token: string} ;
     beforeEach(() => {
         author = requestRegisterAuth('aaa@bbb.com', 'abcde12345', 'Michael', 'Hourn');
         requestAuthLogin('aaa@bbb.com', 'abcde12345');
@@ -258,7 +294,7 @@ describe('Testing POST /v1/admin/quiz', () => {
 ////////////////////        Testing for Removing Quiz       ////////////////////
 
 describe('Testing DELETE /v1/admin/quiz/{quizid}', () => {
-    let author: {token: number}, quiz: {quizId: number};
+    let author: {token: string}, quiz: {quizId: number};
     beforeEach(() => {
         author = requestRegisterAuth('aaa@bbb.com', 'abcde12345', 'Michael', 'Hourn');
         requestAuthLogin('aaa@bbb.com', 'abcde12345');
@@ -276,7 +312,7 @@ describe('Testing DELETE /v1/admin/quiz/{quizid}', () => {
 
         test('Invalid token (does not correlate to given quiz)', () => {
             requestAuthLogout(author.token);
-            const author2: {token: number} = requestRegisterAuth('ccc@ddd.com', '12345abcde', 'John', 'Doe');
+            const author2: {token: string} = requestRegisterAuth('ccc@ddd.com', '12345abcde', 'John', 'Doe');
             requestAuthLogin('ccc@ddd.com', '12345abcde');
             const quiz2: {quizId: number} =  requestQuizCreate(author2.token, 'Quiz Name', '');
             expect(requestQuizRemove(author.token, quiz2.quizId)).toStrictEqual(makeCustomErrorForTest(403))
@@ -351,3 +387,299 @@ describe('Testing DELETE /v1/admin/quiz/{quizid}', () => {
         });
     });
 });
+
+
+////////////////////      Testing for Creating Question     ////////////////////
+
+describe('Testing POST /v1/admin/quiz/{quizid}/question', () => {
+    let author: {token: string}, quiz: {quizId: number};
+    let question: string, duration: number, points: number, answers: Answer[];
+    beforeEach(() => {
+        author = requestRegisterAuth('aaa@bbb.com', 'abcde12345', 'Michael', 'Hourn');
+        requestAuthLogin('aaa@bbb.com', 'abcde12345');
+        quiz = requestQuizCreate(author.token, 'Quiz 1', 'Quiz 1 Des');
+        // Standard values
+        question = 'Question';
+        duration = 1;
+        points = 1;
+        answers = 
+        [{
+            answer: 'Answer 1',
+            correct: true
+        },
+        {
+            answer: 'Answer 2',
+            correct: false
+        }];
+    });
+    
+    
+    describe('Testing: Error cases', () => {
+        // Incorrect values
+        const shortQuestion: string = 'a';
+        const longQuestion: string = '123456789 123456789 123456789 123456789 123456789 123456789';
+        const negativeDuration: number = -1; 
+        const smallPoints: number = 0;
+        const largePoints: number = 11;
+        
+        const oneAnswer: Answer[] = 
+        [{
+            answer: 'Answer',
+            correct: true
+        }];
+        
+        const sevenAnswers: Answer[] = 
+        [{
+            answer: '1', correct: true
+        },{
+            answer: '2', correct: false
+        },{
+            answer: '3', correct: false
+        },{
+            answer: '4', correct: false
+        },{
+            answer: '5', correct: false
+        },{
+            answer: '6', correct: false
+        },{
+            answer: '7', correct: false
+        }];
+
+        const shortAnswer: Answer[] = 
+        [{
+            answer: '',
+            correct: true
+        },
+        {
+            answer: 'Answer 2',
+            correct: false
+        }];
+
+        const longAnswer: Answer[] = 
+        [{
+            answer: '123456789 123456789 123456789 123456789',
+            correct: true
+        },
+        {
+            answer: 'Answer 2',
+            correct: false
+        }];
+
+        const noCorrectAnswers: Answer[] = 
+        [{
+            answer: 'Answer 1',
+            correct: false
+        },
+        {
+            answer: 'Answer 2',
+            correct: false
+        }];
+
+        const duplicateAnswers: Answer[] = 
+        [{
+            answer: 'Answer',
+            correct: true
+        },
+        {
+            answer: 'Answer',
+            correct: false
+        }];
+        
+        test.each([
+            { token: author.token, quizId: quiz.quizId, questionBody: { question: shortQuestion, duration: duration, points: points, answers: answers } }, // Name less than 5 characters
+            { token: author.token, quizId: quiz.quizId, questionBody: { question: longQuestion, duration: duration, points: points, answers: answers } }, // Name greater than 50 characters 
+            { token: author.token, quizId: quiz.quizId, questionBody: { question: question, duration: duration, points: points, answers: oneAnswer } }, // Less than 2 answers
+            { token: author.token, quizId: quiz.quizId, questionBody: { question: question, duration: duration, points: points, answers: sevenAnswers } }, // Greater than 6 answers 
+            { token: author.token, quizId: quiz.quizId, questionBody: { question: question, duration: negativeDuration, points: points, answers: answers } }, // Duration is negative
+            { token: author.token, quizId: quiz.quizId, questionBody: { question: question, duration: duration, points: smallPoints, answers: answers } }, // Points less than 1
+            { token: author.token, quizId: quiz.quizId, questionBody: { question: question, duration: duration, points: largePoints, answers: answers } }, // Points greater than 10 
+            { token: author.token, quizId: quiz.quizId, questionBody: { question: question, duration: duration, points: points, answers: shortAnswer } }, // Answer less than 1 characters
+            { token: author.token, quizId: quiz.quizId, questionBody: { question: question, duration: duration, points: points, answers: longAnswer } }, // Answer greater than 30 characters
+            { token: author.token, quizId: quiz.quizId, questionBody: { question: question, duration: duration, points: points, answers: duplicateAnswers } }, // Duplicate answers 
+        ])('token=$token, quizId=$quizId, question=$question, duration=$duration, points=$points, answers=$answers', ({ token, quizId, questionBody }) => {
+            expect(requestQuestionCreate(token, quizId, { question, duration, points, answers })).toStrictEqual(makeCustomErrorForTest(400));
+        });
+
+        test('Quiz duration is greater than 3 minutes', () => {
+            const duration: number = 100;
+
+            requestQuestionCreate(author.token, quiz.quizId, { question, duration, points, answers });
+            expect(requestQuestionCreate(author.token, quiz.quizId, { question, duration, points, answers })).toStrictEqual(makeCustomErrorForTest(400));
+        });
+
+        test('Invalid token', () => {
+            expect(requestQuestionCreate(author.token + 1, quiz.quizId, { question, duration, points, answers })).toStrictEqual(makeCustomErrorForTest(401));
+        });
+
+        test('User does not own quiz', () => {
+            requestAuthLogout(author.token);
+
+            const author2: {token: string} = requestRegisterAuth('ccc@ddd.com', '12345abcde', 'John', 'Doe');
+            requestAuthLogin('ccc@ddd.com', '12345abcde');
+            expect(requestQuestionCreate(author.token, quiz.quizId, { question, duration, points, answers })).toStrictEqual(makeCustomErrorForTest(403));
+        });
+    });
+
+    describe('Testing: Successful Cases', () => {
+        test('Successfully creates a question', () => {
+            const question1: any = requestQuestionCreate(author.token, quiz.quizId, { question, duration, points, answers});
+            expect(question1.questionId).toStrictEqual(expect.any(Number));
+            expect(requestQuizInfo(author.token, quiz.quizId)).toStrictEqual({
+                quizId: quiz.quizId,
+                name: 'Quiz 1',
+                timeCreated: expect.any(Number),
+                timeLastEdited: expect.any(Number),
+                description: 'Quiz 1 Des',
+                numQuestions: 1,
+                questions: [
+                    {
+                        questionId: question1.questionId,
+                        question: 'Question',
+                        duration: 1,
+                        points: 1,
+                        answers: [
+                            {
+                                answerId: expect.any(Number),
+                                answer: 'Answer 1',
+                                colour: 'red',
+                                correct: true
+                            },
+                            {
+                                answerId: expect.any(Number),
+                                answer: 'Answer 2',
+                                colour: 'red',
+                                correct: false
+                            }
+                        ]
+                    }
+                ]
+            });
+        });
+
+        test.todo('Successfully creates multiple questions', () => {
+            const question1: any = requestQuestionCreate(author.token, quiz.quizId, { question, duration, points, answers});
+            question = 'Question 2';
+            duration = 2;
+            points = 2;
+            const question2: any = requestQuestionCreate(author.token, quiz.quizId, { question, duration, points, answers })
+            expect(requestQuizInfo(author.token, quiz.quizId)).toStrictEqual({
+                quizId: quiz.quizId,
+                name: 'Quiz 1',
+                timeCreated: expect.any(Number),
+                timeLastEdited: expect.any(Number),
+                description: 'Quiz 1 Des',
+                numQuestions: 2,
+                questions: [
+                    {
+                        questionId: question1.questionId,
+                        question: 'Question',
+                        duration: 1,
+                        points: 1,
+                        answers: [
+                            {
+                                answerId: expect.any(Number),
+                                answer: 'Answer 1',
+                                colour: 'red',
+                                correct: true
+                            },
+                            {
+                                answerId: expect.any(Number),
+                                answer: 'Answer 2',
+                                colour: 'red',
+                                correct: false
+                            }
+                        ]
+                    },
+                    {
+                        questionId: question2.questionId,
+                        question: 'Question 2',
+                        duration: 2,
+                        points: 2,
+                        answers: [
+                            {
+                                answerId: expect.any(Number),
+                                answer: 'Answer 1',
+                                colour: 'red',
+                                correct: true
+                            },
+                            {
+                                answerId: expect.any(Number),
+                                answer: 'Answer 2',
+                                colour: 'red',
+                                correct: false
+                            }
+                        ]
+                    }
+                ]
+            });
+        });
+
+        test.todo('Successfully creates questions with another user', () => {
+            requestAuthLogout(author.token);
+
+            const author2: {token: string} = requestRegisterAuth('ccc@ddd.com', '12345abcde', 'John', 'Doe');
+            requestAuthLogin('ccc@ddd.com', '12345abcde');
+
+            const quiz2: {quizId: number} = requestQuizCreate(author2.token, 'Quiz 2', 'Quiz 2 Des');
+            
+            const question1: any = requestQuestionCreate(author2.token, quiz2.quizId, { question, duration, points, answers});
+            question = 'Question 2';
+            duration = 2;
+            points = 2;
+            const question2: any = requestQuestionCreate(author2.token, quiz2.quizId, { question, duration, points, answers })
+            expect(requestQuizInfo(author2.token, quiz2.quizId)).toStrictEqual({
+                quizId: quiz2.quizId,
+                name: 'Quiz 2',
+                timeCreated: expect.any(Number),
+                timeLastEdited: expect.any(Number),
+                description: 'Quiz 1 Des',
+                numQuestions: 2,
+                questions: [
+                    {
+                        questionId: question1.questionId,
+                        question: 'Question',
+                        duration: 1,
+                        points: 1,
+                        answers: [
+                            {
+                                answerId: expect.any(Number),
+                                answer: 'Answer 1',
+                                colour: 'red',
+                                correct: true
+                            },
+                            {
+                                answerId: expect.any(Number),
+                                answer: 'Answer 2',
+                                colour: 'red',
+                                correct: false
+                            }
+                        ]
+                    },
+                    {
+                        questionId: question2.questionId,
+                        question: 'Question',
+                        duration: 2,
+                        points: 2,
+                        answers: [
+                            {
+                                answerId: expect.any(Number),
+                                answer: 'Answer 1',
+                                colour: 'red',
+                                correct: true
+                            },
+                            {
+                                answerId: expect.any(Number),
+                                answer: 'Answer 2',
+                                colour: 'red',
+                                correct: false
+                            }
+                        ]
+                    }
+                ]
+            });
+        });
+    });
+});
+
+
+////////////////////      Testing for Removing Question     ////////////////////
